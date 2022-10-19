60b57e90b0bd5d9f55198c68bc2485eb26ba6abd
Camden <58599428+Condensed-Air@users.noreply.github.com>
2022-10-19 11:01:18 -0500

    Upload build

---
+++ Shapes.js
@@ -0,0 +1,94 @@
+var SHAPES = {
+    i: {
+	spin: 'corner',
+	startX: 5,
+	startY: 0,
+	pos: [
+	    { x: -2, y: -1 },
+	    { x: -1, y: -1},
+	    { x: 0, y: -1 },
+	    { x: 1, y: -1 }
+	],
+	image: 'cyanblock.png',
+	kickType: 'i_block'
+    },
+    o: {
+	spin: 'corner',
+	startX: 5,
+	startY: -1,
+	pos: [
+	    { x: -1, y: 0 },
+	    { x: 0, y: 0},
+	    { x: -1, y: -1 },
+	    { x: 0, y: -1 }
+	],
+	image: 'yellowblock.png',
+	kickType: 'standard'
+    },
+    j: {
+	spin: 'block',
+	startX: 4,
+	startY: -1,
+	pos: [
+	    { x: -1, y: -1 },
+	    { x: -1, y: 0 },
+	    { x: 0, y: 0 },
+	    { x: 1, y: 0 }
+	],
+	image: 'blueblock.png',
+	kickType: 'standard'
+    },
+    l: {
+	spin: 'block',
+	startX: 4,
+	startY: -1,
+	pos: [
+	    { x: -1, y: 0 },
+	    { x: 0, y: 0 },
+	    { x: 1, y: 0 },
+	    { x: 1, y: -1 }
+	],
+	image: 'orangeblock.png',
+	kickType: 'standard'
+    },
+    s: {
+	spin: 'block',
+	startX: 4,
+	startY: -1,
+	pos: [
+	    { x: -1, y: 0 },
+	    { x: 0, y: 0 },
+	    { x: 0, y: -1 },
+	    { x: 1, y: -1 }
+	],
+	image: 'greenblock.png',
+	kickType: 'standard'
+    },
+    z: {
+	spin: 'block',
+	startX: 4,
+	startY: -1,
+	pos: [
+	    { x: -1, y: -1 },
+	    { x: 0, y: -1 },
+	    { x: 0, y: 0 },
+	    { x: 1, y: 0 }
+	],
+	image: 'redblock.png',
+	kickType: 'standard'
+    },
+    t: {
+	spin: 'block',
+	startX: 4,
+	startY: -1,
+	pos: [
+	    { x: -1, y: 0 },
+	    { x: 0, y: 0 },
+	    { x: 0, y: -1 },
+	    { x: 1, y: 0 }
+	],
+	image: 'purpleblock.png',
+	kickType: 'standard'
+    }
+    
+};
